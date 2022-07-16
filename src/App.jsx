ecb7129ea23f43c462bb9264609553a80e788f18
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-16 19:17:22 +0530

    deploy: HashRouter inplace of BrowserRouter and other deployment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList'
 import Product from './pages/Product';
